cc75c3f28f4c971ce9a13458710e7537df64ba63
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-24 11:08:20 +0100

    add run method -> compile + execute, modify methods for argument array compatibility, output tokens, ast, generated code only if --debug option is set

--- lib/cake_lang/cli.rb
+++ lib/cake_lang/cli.rb
@@ -1,27 +1,53 @@
-filename = ARGV[0].split('.')[0]
+module CakeLang
+  module CLI
+    class << self
+      # Handle unavailable methods
+      # @param [String] name [called Methodname]
+      # @param [Array] *args [Available args]
+      def method_missing(name, *args)
+        puts "Command #{name} not available"
+        print "Available Commands are: \n\n"
+        self.methods(false).each do |method|
+          print "\t #{method}\n" unless method == :method_missing
+        end
+        print "\n"
+      end
 
-input = ""
-input_array = []
-tokens = []
+      def compile(args)
+        file = args[0]
+        option = args[1]
 
-File.open(ARGV[0], "r") do |f|
-  input = f.read
+        input = ""
+        input_array = []
+        tokens = []
+
+        File.open(file, "r") do |f|
+          input = f.read
+        end
+
+        tokens = CakeLang::Lexer.new.lex(input.strip)
+        ast = CakeLang::Parser.new.parse(tokens.dup)
+        code = CakeLang::CodeGenerator::C.compile(ast)
+
+        if option.eql?('--debug')
+          puts "Tokenized input:\n"
+          puts "#{tokens.inspect}\n\n"
+          puts "Abstract Syntax Tree:\n"
+          puts "#{ast.inspect}\n\n"
+          puts "Generated Code:\n"
+          puts "#{code.inspect}\n\n"
+        end
+
+        @filename = file.split('.')[0]
+        system("echo '#{code}' > #{@filename}.c")
+        system("gcc -o #{@filename} #{@filename}.c")
+      end
+
+      def run(args)
+        compile(args)
+        system("./#{@filename}")
+      end
+    end
+  end
 end
 
-tokens = CakeLang::Lexer.new.lex(input.strip)
-
-puts "Tokenized input:\n"
-puts "#{tokens.inspect}\n\n"
-
-ast = CakeLang::Parser.new.parse(tokens)
-
-puts "Abstract Syntax Tree:\n"
-puts "#{ast.inspect}\n\n"
-
-code = CakeLang::CodeGenerator::C.compile(ast)
-
-puts "Generated Code:\n"
-puts "#{code.inspect}\n\n"
-
-system("echo '#{code}' > #{filename}.c")
-system("gcc -o #{filename} #{filename}.c")
