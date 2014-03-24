401ef6c7a68d7c4990d60d9722e21a83d588f210
Benny1992 <klotz.benjamin@yahoo.de>
2014-03-24 07:16:56 +0100

    fixed code gen typo

--- lib/cake_lang.rb
+++ lib/cake_lang.rb
@@ -1,7 +1,7 @@
 require 'cake_lang/version'
 require 'cake_lang/lexer'
 require 'cake_lang/parser'
-require 'cake_lang/code_gen'
+require 'cake_lang/code_generator'
 
 module CakeLang
   # Your code goes here...
